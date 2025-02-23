3f1d02e89dce5bb372702e80a71a678bd50994cb
websurf500 <websurf500@gmail.com>
2025-02-23 19:50:47 +0530

    Update EcommerceBackendApplication.java

--- src/main/java/org/scopeindia/ecomm/EcommerceBackendApplication.java
+++ src/main/java/org/scopeindia/ecomm/EcommerceBackendApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "org.scopeindia.ecomm")	
 public class EcommerceBackendApplication {
 
 	public static void main(String[] args) {
